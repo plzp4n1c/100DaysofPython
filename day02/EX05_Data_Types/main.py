c300d698cf048d26570f3f960764154afbe6bd68
plzp4n1c <plzp4n1c@proton.me>
2023-10-15 12:01:29 +0100

    Day 2

---
+++ day02/EX05_Data_Types/main.py
@@ -0,0 +1,4 @@
+# Day 2 - Data Types
+
+two_digit_number = input()
+print(int(two_digit_number[0]) + int(two_digit_number[1]))
---
+++ day02/EX06_BMI_Calculator/main.py
@@ -0,0 +1,6 @@
+# Day 2 - Calculating BMI
+
+height = input()
+weight = input()
+bmi = int(weight) / float(height)**2
+print(int(bmi))
